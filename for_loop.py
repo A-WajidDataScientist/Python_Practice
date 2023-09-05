c1f1da84bcd01827c3b3f2388a313cc69d8c7734
A-WajidDataScientist <20cs79@students.muet.edu.pk>
2023-09-05 23:11:20 +0500

    for loop practice

---
+++ for_loop.py
@@ -0,0 +1,14 @@
+# Range function
+# print(list(range(15)))
+
+# Sequence
+# String
+
+# for i in range(15):
+# print(i)
+
+number = int(input("enter number = "))
+
+for i in range(1, 11):
+
+    print(number, "*", i, "=", number*i)
