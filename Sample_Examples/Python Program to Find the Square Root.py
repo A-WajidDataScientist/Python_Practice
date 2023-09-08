62bb5ed40e55c3f5e31b6ae5028c0ac6623cc329
A-WajidDataScientist <20cs79@students.muet.edu.pk>
2023-09-09 01:11:10 +0500

    Pratice_Example

--- Sample_Examples/Python Program to Find the Square Root.py
+++ Sample_Examples/Python Program to Find the Square Root.py
@@ -1,5 +1,20 @@
-# Python Program to Find the Square Root
+# # Python Program to Find the Square
 
+# a = 1
+# while (a == 1 or a != 1) and a != 0:
+#     a = int(input("Enter 1 for continue Or 0 for exist "))
+
+#     if (a == 1 or a != 1) and a != 0:
+
+#         num = int(input("Enter number num = "))
+#         sq = num * num
+#         print("square = ", sq)
+
+#     else:
+#         print("calculation ended")
+
+# Python Program to Find the Square Root
+import math
 a = 1
 while (a == 1 or a != 1) and a != 0:
     a = int(input("Enter 1 for continue Or 0 for exist "))
@@ -7,8 +22,7 @@ while (a == 1 or a != 1) and a != 0:
     if (a == 1 or a != 1) and a != 0:
 
         num = int(input("Enter number num = "))
-        sq = num * num
-        print("square = ", sq)
+        print("square root = ", math.sqrt(num))
 
     else:
         print("calculation ended")
