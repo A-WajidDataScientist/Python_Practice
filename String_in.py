dc3291e9196f315afb48b014748bbb3d6f406bdc
A-WajidDataScientist <20cs79@students.muet.edu.pk>
2023-09-08 21:34:44 +0500

    String_in_python

--- String_in.py
+++ String_in.py
@@ -1,3 +1,13 @@
-#Creating String
-c=str("hello")
+# Creating String
+c = str("hello")
+print(c)
+c = str('hello')
+print(c)
+c = str("""hello""")  # this is use for multi_lines
+print(c)
+c = str('''hello''')
+print(c)
+c = str("he'llo")
+print(c)
+c = str('"hello"')
 print(c)
