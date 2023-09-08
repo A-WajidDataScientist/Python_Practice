5fc28e99ad860f8ac19f96ed35790f52d245867c
A-WajidDataScientist <20cs79@students.muet.edu.pk>
2023-09-09 01:02:19 +0500

    Practice_Examples

---
+++ Sample_Examples/Python Program to Add Two Numbers.py
@@ -0,0 +1,31 @@
+# Python Program to Add Two Numbers
+
+a = 3
+b = 5
+c = a+b
+print(c)
+
+# OR
+
+print(3+9)
+
+# OR
+
+# num_1 = int(input("Enter number num_1= "))
+# num_2 = int(input("Enter number num_2= "))
+# sum = num_1 + num_2
+# print("sum = ", sum)
+
+# OR
+a = 1
+while a == 1:
+    a = int(input("Enter 1 for continue Or 0 for exist "))
+    if a == 1:
+
+        num_1 = int(input("Enter number num_1= "))
+        num_2 = int(input("Enter number num_2= "))
+        sum = num_1 + num_2
+        print("sum = ", sum)
+
+    else:
+        print("calculation ended")
---
+++ Sample_Examples/Python Program to Find the Square Root.py
@@ -0,0 +1,14 @@
+# Python Program to Find the Square Root
+
+a = 1
+while (a == 1 or a != 1) and a != 0:
+    a = int(input("Enter 1 for continue Or 0 for exist "))
+
+    if (a == 1 or a != 1) and a != 0:
+
+        num = int(input("Enter number num = "))
+        sq = num * num
+        print("square = ", sq)
+
+    else:
+        print("calculation ended")
