a64cbf42fd748725a0576a42c7de545e75697acd
A-WajidDataScientist <20cs79@students.muet.edu.pk>
2023-09-11 22:55:38 +0500

    Variables_Practice

---
+++ Variable.py
@@ -0,0 +1,34 @@
+# Exercise: Python Variables
+
+# Create a variable called break and assign it a value 5. See what happens and find out the reason behind the behavior that you see.
+
+# Create two variables. One to store your birth year and another one to store current year. Now calculate your age using these two variables
+
+# Store your first, middle and last name in three different variables and then print your full name using these variables
+
+# Answer which of these are invalid variable names: _nation 1record record1 record_one record-one record^one continue
+
+# Q1 break = 5 #break key it doesn't used for variable name
+
+# Q2
+brith_year = 2000
+current_year = 2023
+
+my_age = current_year - brith_year
+
+print(my_age)
+
+# Q3
+first_name = "Abdul"
+second_name = "Wajid"
+last_name = "Ansari"
+print(first_name, second_name, last_name)
+
+# Q4
+# _nation: valid
+#  1record: invalid
+#  record1: valid
+#  record_one: valid
+#  record-one: invalid
+#  record^one: invalid
+#  continue: invalid
