c39cb0cdeaffc5a23b5ecf2dbb84f0a623df56b8
A-WajidDataScientist <20cs79@students.muet.edu.pk>
2023-09-11 20:46:23 +0500

    Tuples_practice

--- Tuples.py
+++ Tuples.py
@@ -1,8 +1,12 @@
-# Create
+# Create Tuple
 t_1 = ()  # empty tuple
+
+t_2 = (1,)  # single tuple create ","
 
 t_2 = (1, 2, 3, 4, 5)  # same type of data type called as homogenous tuple
 
 t_3 = ("hello", 2, 2.5)  # heterogenous tuple
 
 print(t_1, t_2, t_3)
+
+# no edit or add in tuple
