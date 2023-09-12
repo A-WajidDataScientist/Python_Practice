09f6adbdf0a1a608f61ad032ee66d89577249728
A-WajidDataScientist <20cs79@students.muet.edu.pk>
2023-09-12 21:03:57 +0500

    String_Practice

--- String_in.py
+++ String_in.py
@@ -78,3 +78,55 @@ print("it is raining".find("x"))
 print("it is raining".index("a"))
 
 #
+print(dir(str))
+
+# Exercise: String in Python
+# Q1. Create 3 variables to store street, city and country, now create address variable to store entire address.
+# Use two ways of creating this variable, one using + operator and the other using f-string.
+# Now Print the address in such a way that the street, city and country prints in a separate line
+# Q2. Create a variable to store the string "Earth revolves around the sun"
+# Q3. Print "revolves" using slice operator
+# Q4. Print "sun" using negative index
+# Q5. Create two variables to store how many fruits and vegetables you eat in a day.
+# Now Print "I eat x veggies and y fruits daily" where x and y presents vegetables and fruits that you eat everyday. Use python f string for this.
+
+# sollution:
+# Q1
+street = "Street: Raza-Bazaar "
+city = "City: chak "
+country = "Pakistan"
+address = street+city+country
+print(address)
+print(f"Address: {street}{city}{country}")
+
+
+# Q2
+txt = "Earth revolves around the sun"
+print(txt)
+
+# Q3
+print(txt[6:15])
+
+# Q4
+print(txt[-3:])
+
+# Q5
+y = "2"
+x = "3"
+
+print(f"I eat {x} veggies and {y} fruits daily")
+
+
+# Q6. I have a string variable called s='maine 200 banana khaye'.
+# This of course is a wrong statement, the correct statement is 'maine 10 samosa khaye'.
+# Replace incorrect words in original strong with new ones and print the new string. Also try to do this in one line.
+
+s = 'maine 200 banana khaye'
+s = s.replace(" 200", " 10")
+s = s.replace(" banana", " samosa")
+print(s)
+
+
+s = s.replace(" 200 banana", " 10 samosa")
+
+print(s)
